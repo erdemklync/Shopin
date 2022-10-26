bd6fe8bf7611550d7930c3ed9233fe34388fbf11
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-10-26 12:09:30 +0300

    Create Store Api

--- app/src/main/java/com/erdemklync/shopin/data/remote/service/StoreApi.kt
+++ app/src/main/java/com/erdemklync/shopin/data/remote/service/StoreApi.kt
@@ -1,10 +1,9 @@
 package com.erdemklync.shopin.data.remote.service
 
 import com.erdemklync.shopin.data.remote.entity.Product
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface StoreApi {
     @GET("products")
-    fun getProducts(): Call<List<Product>>
+    suspend fun getProducts(): List<Product>
 }
