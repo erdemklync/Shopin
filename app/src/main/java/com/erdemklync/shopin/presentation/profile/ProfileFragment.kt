0057b0d41017d17379aee2e0942177eea3c43fad
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-11-02 12:48:25 +0300

    Design Profile screen

---
+++ app/src/main/java/com/erdemklync/shopin/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,32 @@
+package com.erdemklync.shopin.presentation.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.erdemklync.shopin.databinding.FragmentProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileFragment : Fragment() {
+
+    private val viewModel: ProfileViewModel by viewModels()
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
