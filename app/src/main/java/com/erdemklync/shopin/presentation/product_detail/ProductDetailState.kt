6ba9ad1da223f5fc30c89796b78c866c1b66a2ee
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-11-06 23:23:19 +0300

    Edit xml layout references

--- app/src/main/java/com/erdemklync/shopin/presentation/product_detail/ProductDetailState.kt
+++
@@ -1,9 +0,0 @@
-package com.erdemklync.shopin.presentation.product_detail
-
-import com.erdemklync.shopin.data.remote.entity.Product
-
-data class ProductDetailState(
-    //val dataState: DataState<Product> = DataState.Loading(),
-    val product: Product? = null,
-    val amount: Int = 1,
-)
